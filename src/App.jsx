d2f06ac80bc614b23e3bef4ad361f3ba800be233
Mac <mac@MusfiQs-MacBook-Air.local>
2024-07-10 18:03:11 +0600

    product add page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
-  return <div>App page</div>;
+  return (
+    <div>
+      <Navbar></Navbar>
+      <hr />
+      <div className="app-content">
+        <Sidebar></Sidebar>
+        <Outlet></Outlet>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+import Add from "./pages/Add/Add.jsx";
+import List from "./pages/List/List.jsx";
+import Orders from "./pages/Orders/Orders.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    children: [
+      {
+        path: "/add",
+        element: <Add></Add>,
+      },
+      {
+        path: "/list",
+        element: <List></List>,
+      },
+      {
+        path: "/orders",
+        element: <Orders></Orders>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
